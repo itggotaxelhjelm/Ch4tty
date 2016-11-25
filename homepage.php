a09ddfc20740f74346499f22eac3273630093a77
itggotaxelhjelm <Axel.hjelm@itggot.se>
2016-11-25 13:40:44 +0100

    inital commit

---
+++ homepage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+
+<html>
+	<head>
+		<title>Page Title</title>
+	</head>
+	<body>
+
+		<link type="text/css" rel="stylesheet" href="styles.css" />
+
+		<div class="chat_main">
+			<h3>Chat</h3>
+		</div>
+
+
+			<form class="submit_form" method="post" action="main.php">
+				<input type="text" name="s_message" />
+				<input type="submit" value="Say" name="s_say" />
+			</form>
+
+
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<meta charset="UTF-8">
+<h1>Ch4tty</h1>
+<form action="index.php" id="form" method="post">
+	<input type="text" name="user" placeholder="Write user here!"> <br/>
+	<input type="password" name="password" placeholder="Write password here">
+	<input type="submit" name="login" value="Log in">
+</form>
+
+
+<?php
+session_start();
+
+if(isset($_POST['login'])) //kollar om login finns i urln
+{
+	$user = $_POST['user'];
+	$password = $_POST['password'];
+
+	$correctUser = 'Axel';
+	$correctPassword = '123';
+
+	if($user == $correctUser && $password == $correctPassword)
+	{
+		$_SESSION['login'] = 'ok';
+		header('location: homepage.php');
+	}
+	else
+	{
+		echo "The username or password doesn't match";
+	}
+}
+
+?>
